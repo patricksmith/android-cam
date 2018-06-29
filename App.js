fda341c678af434a783a7fc07bc98ea022af57bd
Patrick Smith <patrick@promptworks.com>
2018-06-29 15:46:07 -0400

    Add camera component

--- App.js
+++ App.js
@@ -1,16 +1,48 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { RNCamera } from 'react-native-camera';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <RNCamera
+          ref={ref => {
+            this.camera = ref;
+          }}
+          style = {styles.preview}
+          type={RNCamera.Constants.Type.back}
+          permissionDialogTitle={'Permission to use camera'}
+          permissionDialogMessage={'We need your permission to use your camera phone'}
+          onCameraReady={() => console.log('My camera is ready')}
+          onMountError={err => console.log(`Failed to mount ${err}`)}
+          onBarCodeRead={evt => {
+            console.log(`Got type "${evt.type}" and data "${evt.data}"`)
+          }}
+          barCodeTypes={[RNCamera.Constants.BarCodeType.qr]}
+        />
+        <View style={{flex: 0, flexDirection: 'row', justifyContent: 'center',}}>
+          <TouchableOpacity
+            onPress={this.takePicture.bind(this)}
+            style = {styles.capture}
+          >
+            <Text style={{fontSize: 14}}> SNAP </Text>
+          </TouchableOpacity>
+        </View>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
     );
   }
+
+  takePicture = async function() {
+    if (this.camera) {
+      const options = { quality: 0.5, base64: true };
+      const data = await this.camera.takePictureAsync(options)
+      console.log(data.uri);
+    }
+  };
 }
 
 const styles = StyleSheet.create({
@@ -20,4 +52,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20
+  }
 });
